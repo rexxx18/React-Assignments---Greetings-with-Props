3af9eda40f600ca3086e204e8ec03503b38f47e2
rexxx18 <65245341+rexxx18@users.noreply.github.com>
2020-11-09 10:06:17 +0530

    changes

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,8 @@ import React from "react";
 function Welcome(props) {
   return (
     <>
-      <h1>{`Hey${props.name}!`}</h1>
+      <h1>Hey!</h1>
+      <h1>{props.name}</h1>
       <h2>{"Welcome to Newton School."}</h2>
     </>
   );
